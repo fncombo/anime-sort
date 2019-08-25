d77399036215c84d58ab5c20f671ae535a1375f7
Eugene <fncombo@gmail.com>
2019-08-25 19:36:17 +0100

    Update App.js

--- src/js/App.js
+++ src/js/App.js
@@ -103,11 +103,11 @@ function App() {
                 <p><strong>How does this work?</strong></p>
                 <p>
                     This app uses the <a href="https://en.wikipedia.org/wiki/Elo_rating_system" target="_blank" rel="noopener noreferrer">Elo rating system</a> to
-                    sort anime by continuously comparing two anime against each other. Each anime gains and loses ranking points based on which anime it won against or lost to.
+                    sort anime by continuously comparing two anime against each other. Each anime gains and loses rank points based on which anime it won against or lost to.
                 </p>
                 <p>
-                    Due to the exponential nature of this method, to significantly reduce the number of comparisons you need to manually make for very large sets of anime,
-                    losers of the loser automatically lose to the winner, similarly, winners over the winner win over the loser.
+                    Due to the exponential nature of this sort, to significantly reduce the number of comparisons you need to manually make for very large sets of anime,
+                    losers of the loser automatically lose to the winner, similarly, winners over the winner win against the loser.
                 </p>
                 <p className="pre">
                     In other words, if you choose <span>Anime A</span> over <span>Anime B</span>, and then choose <span>Anime B</span> over <span>Anime C</span> then <span>Anime A</span> and
@@ -115,7 +115,7 @@ function App() {
                 </p>
                 <p>
                     During sorting, you will see a significant increase in total sorting progress as you approach the middle, and it will slow down again towards the very end.
-                    This should take about 5 to 15 minutes if you have up to 100 completed anime, 20 to 30 minutes if you have up to 200 anime, and over 30 minutes if you have over 200 anime.
+                    This should take about 5 to 15 minutes to do if you have up to 100 completed anime, 20 to 30 minutes if you have up to 200 anime, and over 30 minutes if you have over 200 anime.
                 </p>
                 <p>
                     View source code on <a href="https://github.com/fncombo/animesort" target="_blank" rel="noopener noreferrer">GitHub</a>.
