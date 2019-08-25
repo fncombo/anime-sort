11cb5c6737ac45bb105919142c0951ab419919b5
Eugene <fncombo@gmail.com>
2019-08-25 18:28:59 +0100

    More data on gallery screen

--- src/js/ResultsGallery.js
+++ src/js/ResultsGallery.js
@@ -11,7 +11,14 @@ import '../scss/ResultsGallery.scss'
  * Show a gallery of all the anime sorted by Elo.
  */
 function ResultsGallery() {
-    const { state: { animeObject, anime } } = useContext(GlobalState)
+    const { state: {
+        animeObject,
+        anime,
+        totalInitialPairs,
+        manuallyEliminatedCount,
+        autoEliminatedCountA,
+        autoEliminatedCountB,
+    } } = useContext(GlobalState)
 
     const allAnime = Object.entries(anime)
 
@@ -19,6 +26,9 @@ function ResultsGallery() {
         <>
             <div className="container is-column">
                 <h1>Here is your sorted anime, enjoy!</h1>
+                <p><strong>Comparison decisions you've made:</strong> {manuallyEliminatedCount.toLocaleString()}</p>
+                <p><strong>Automatic decisions made:</strong> {(autoEliminatedCountA + autoEliminatedCountB).toLocaleString()}</p>
+                <p><strong>Total pairs of anime compared:</strong> {totalInitialPairs.toLocaleString()}</p>
             </div>
             <div className="gallery">
                 {allAnime.sort(([ , { elo: aElo} ], [ , { elo: bElo } ]) => {
