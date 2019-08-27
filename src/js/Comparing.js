0ffff0d745082bde29ddf1b88d0d837118c560c2
Eugene <fncombo@gmail.com>
2019-08-27 17:18:53 +0100

    Transition

--- src/js/Comparing.js
+++ src/js/Comparing.js
@@ -44,11 +44,15 @@ function CompareAnime() {
 
     // Callback to choose which anime wins
     const chooseWinner = (winnerId, loserId) => {
-        dispatch({
-            type: ACTIONS.UPDATE_PAIR,
-            winnerId,
-            loserId,
-        })
+        document.body.classList.add('is-changing')
+
+        setTimeout(() => {
+            dispatch({
+                type: ACTIONS.UPDATE_PAIR,
+                winnerId,
+                loserId,
+            })
+        }, 150)
     }
 
     // Undo previous selection and return to it, enabling to change the answer
@@ -94,6 +98,10 @@ function CompareAnime() {
             // Bind event to choose an anime using the arrow keys
             window.addEventListener('keyup', keyHandler)
 
+            setTimeout(() => {
+                document.body.classList.remove('is-changing')
+            }, 75)
+
             // Unbind event on re-render
             return () => {
                 window.removeEventListener('keyup', keyHandler)
@@ -104,59 +112,87 @@ function CompareAnime() {
         dispatch({ type: ACTIONS.GET_PAIR })
     })
 
-    // Wait until next comparison
-    if (!isComparing) {
-        return null
-    }
-
     const allAnime = Object.entries(anime)
     const progress = ((totalInitialPairs - totalRemainingPairs) / totalInitialPairs) * 100
+
+    // Wait until next comparison
+    if (!isComparing) {
+        return (
+            <>
+                <div className="container is-column">
+                    {/* <h1>Which anime is better?</h1> */}
+                    <p class="comparison-subtitle">Choose by clicking on the image or using the left and right arrow keys.</p>
+                    <div className="comparison-stage"></div>
+                </div>
+                <div className="container is-column">
+                    <div className="comparison-buttons is-border">
+                        {previousState && <button onClick={undo}>Undo last comparison</button>}
+                        {!isSaved && <button onClick={save}>Save progress</button>}
+                    </div>
+                    <div className="comparison-row">
+                        <div className="progress-container">
+                            <p><strong>Total completed anime to sort:</strong> {allAnime.length}</p>
+                            <p><strong>Sorting progress:</strong> {progress.toFixed(2).toLocaleString()}%</p>
+                            <div className="progress">
+                                <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+                            </div>
+                            <p><strong>Comparison decisions you've made:</strong> {manuallyEliminatedCount.toLocaleString()}</p>
+                            <p><strong>Automatic decisions made:</strong> {(autoEliminatedCountA + autoEliminatedCountB).toLocaleString()}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     const left = animeObject[leftId]
     const right = animeObject[rightId]
 
     return (
         <>
             <div className="container is-column">
-                <h1>Which anime is better?</h1>
-                <p>Choose by clicking on the image or using the left and right arrow keys.</p>
-                <div className="comparison-row is-border">
-                    <div className="comparison-col">
-                        <h2>{left.title}</h2>
-                    </div>
-                    <div className="comparison-col">
-                        <h2>{right.title}</h2>
-                    </div>
-                </div>
-                <div className="comparison-row">
-                    <div className="comparison-col">
-                        <div className="comparison-button" onClick={() => chooseWinner(leftId, rightId)}>
-                            <img src={left.image_url} height={350} width={250} alt={right.title} />
+                {/* <h1>Which anime is better?</h1> */}
+                <p class="comparison-subtitle">Choose by clicking on the image or using the left and right arrow keys.</p>
+                <div className="comparison-stage">
+                    <div className="comparison-row">
+                        <div className="comparison-col">
+                            <h2>{left.title}</h2>
+                        </div>
+                        <div className="comparison-col">
+                            <h2>{right.title}</h2>
                         </div>
                     </div>
-                    <div className="comparison-col">
-                        <div className="comparison-button" onClick={() => chooseWinner(rightId, leftId)}>
-                            <img src={right.image_url} height={350} width={250} alt={right.title} />
+                    <div className="comparison-row">
+                        <div className="comparison-col">
+                            <div className="comparison-button" onClick={() => chooseWinner(leftId, rightId)}>
+                                <img src={left.image_url} height={350} width={250} alt={right.title} />
+                            </div>
+                        </div>
+                        <div className="comparison-col">
+                            <div className="comparison-button" onClick={() => chooseWinner(rightId, leftId)}>
+                                <img src={right.image_url} height={350} width={250} alt={right.title} />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="comparison-row">
-                    <div className="comparison-col is-normal">
-                        <p><strong>Your rating on MyAnimeList:</strong> {left.score}</p>
-                        <p><strong>Type:</strong> {left.type}{left.total_episodes > 1 ? <> &ndash; {left.total_episodes} episodes</> : ''}</p>
-                        <p>
-                            <a href={left.url} target="_blank" rel="noopener noreferrer">
-                                Open on MyAnimeList
-                            </a>
-                        </p>
-                    </div>
-                    <div className="comparison-col is-normal">
-                        <p><strong>Your rating on MyAnimeList:</strong> {right.score}</p>
-                        <p><strong>Type:</strong> {right.type}{right.total_episodes > 1 ? <> &ndash; {right.total_episodes} episodes</> : ''}</p>
-                        <p>
-                            <a href={right.url} target="_blank" rel="noopener noreferrer">
-                                Open on MyAnimeList
-                            </a>
-                        </p>
+                    <div className="comparison-row">
+                        <div className="comparison-col is-normal">
+                            <p><strong>Your rating on MyAnimeList:</strong> {left.score}</p>
+                            <p><strong>Type:</strong> {left.type}{left.total_episodes > 1 ? <> &ndash; {left.total_episodes} episodes</> : ''}</p>
+                            <p>
+                                <a href={left.url} target="_blank" rel="noopener noreferrer">
+                                    Open on MyAnimeList
+                                </a>
+                            </p>
+                        </div>
+                        <div className="comparison-col is-normal">
+                            <p><strong>Your rating on MyAnimeList:</strong> {right.score}</p>
+                            <p><strong>Type:</strong> {right.type}{right.total_episodes > 1 ? <> &ndash; {right.total_episodes} episodes</> : ''}</p>
+                            <p>
+                                <a href={right.url} target="_blank" rel="noopener noreferrer">
+                                    Open on MyAnimeList
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
